08c829db301837e076fd1e7349b738ef3c539e9e
HarshitJoshi281 <28harshit98@gmail.com>
2026-06-29 18:21:06 +0530

    Time: 5 ms (89.08%), Space: 175.1 MB (71.07%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] getAverages(int[] nums, int k) {
         int[] result = new int[nums.length];
-        long[] sum = new long[nums.length];
-        sum[0]=nums[0];
-        for(int i =1;i<nums.length;i++){
-            sum[i]=sum[i-1]+nums[i];
+        Arrays.fill(result,-1);
+        long sum =0;
+        if(k==0)return nums;
+        int window = (2*k)+1;
+        if(window>nums.length)return result;
+        for(int i =0;i<window;i++){
+            sum+=nums[i];
         }
-        long avj =0;
-        for(int i =0;i<nums.length;i++){
-            if(i-k<0||i+k>nums.length-1){
-                result[i]=-1;
-            }
-            else{
-                if(i-k==0){
-                    avj= sum[i+k]/((2*k)+1);
-                    result[i]= (int)avj;
-                    avj=0;
-                }
-                else{
-                    avj= (sum[i+k]-sum[i-k-1])/((2*k)+1);
-                    result[i]= (int)avj;
-                    avj=0;
-                }
-            }
-
+        result[k]=(int) (sum/window);
+        for (int i = window; i < nums.length; i++) {
+            sum += nums[i];
+            sum -= nums[i - window];
+            result[i - k] = (int)(sum / window);
         }
         return result;
+
     }
 }
