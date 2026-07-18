28fa8534e44f540c22c4ae809704b010a570deb0
HarshitJoshi281 <28harshit98@gmail.com>
2026-07-18 12:53:28 +0530

    Time: 2 ms (99.42%), Space: 54.8 MB (79.07%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.java
@@ -1,41 +1,33 @@
 class Solution {
     public int countServers(int[][] grid) {
-        int count =0;
-        for(int i =0;i<grid.length;i++){
-            int val =0;
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    val++;
-                
-                    }
-                
-            }
-            if(val>1){
-                count+=val;
-                for(int k=0;k<grid[0].length;k++){
-                    if(grid[i][k]==1)
-                    grid[i][k]=2;
+
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+       
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    row[i]++;
+                    col[j]++;
                 }
             }
-            val=0;
         }
-        for(int i =0;i<grid[0].length;i++){
-        int val=0;
-        int val2 =0;
-        for(int j=0;j<grid.length;j++){
-            if(grid[j][i]>=1)val++;
-            if(grid[j][i]==2)val2++;
+
+        int count = 0;
+
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && (row[i] > 1 || col[j] > 1)) {
+                    count++;
+                }
+            }
         }
-        if(val>1)count+=val-val2;
-        val=0;
-        val2=0;
 
+        return count;
     }
-       return count;
-    }
-    
-
-   
-
-    
 }
