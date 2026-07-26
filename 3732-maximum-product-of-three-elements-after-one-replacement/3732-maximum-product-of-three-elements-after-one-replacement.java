18d20af82449fbd0291deb63b9bc1dcfbe635167
HarshitJoshi281 <28harshit98@gmail.com>
2026-07-26 12:01:06 +0530

    Time: 5 ms (81.03%), Space: 97.1 MB (36.78%) - LeetHub

--- 3732-maximum-product-of-three-elements-after-one-replacement/3732-maximum-product-of-three-elements-after-one-replacement.java
+++ 3732-maximum-product-of-three-elements-after-one-replacement/3732-maximum-product-of-three-elements-after-one-replacement.java
@@ -1,11 +1,32 @@
 class Solution {
     public long maxProduct(int[] nums) {
+
+        
         for(int i =0;i<nums.length;i++){
             nums[i]= Math.abs(nums[i]);
         }
-        Arrays.sort(nums);
+        long f=0;
+        long s=0;
+        if(nums[0]>nums[1]){
+            f=nums[0];
+            s=nums[1];
+        }
+        else{
+            f=nums[1];
+            s=nums[0];
+        }
+        for(int i= 2;i<nums.length;i++){
+            if(nums[i]>f){
+                s=f;
+                f=nums[i];
+            }
+            else if(nums[i]>s){
+                s=nums[i];
+            }
+        }
+       
         
-        long result = (long) nums[nums.length-1]* (long)nums[nums.length-2]*100000;
+        long result = f*s *100000;
         return result;
     }
 }
