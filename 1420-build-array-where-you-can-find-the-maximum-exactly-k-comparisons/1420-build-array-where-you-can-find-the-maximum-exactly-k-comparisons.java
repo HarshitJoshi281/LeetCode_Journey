e92ccb8da26fab0ddd3d00374dc008df567b6e91
HarshitJoshi281 <28harshit98@gmail.com>
2026-08-01 20:03:34 +0530

    Time: 126 ms (47.75%), Space: 46.4 MB (40.41%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.java
@@ -2,16 +2,12 @@ class Solution {
     int MOD = (int)1e9+7;
 
     public int numOfArrays(int n, int m, int k) {
-        int[][][] dp = new int[n][m + 1][k + 1];
-        for (int[][] layer : dp) {
-            for (int[] row : layer) {
-                Arrays.fill(row, -1);
-            }
-        }
+        Integer[][][] dp = new Integer[n][m + 1][k + 1];
+       
         return help(n, m, 0, k, 0, 0, dp);
     }
 
-    public int help(int n, int m, int maxSofar, int k, int idx, int SearchCost, int[][][] dp) {
+    public int help(int n, int m, int maxSofar, int k, int idx, int SearchCost, Integer[][][] dp) {
         
         if (idx == n) {
             return SearchCost == k ? 1 : 0;
@@ -19,15 +15,15 @@ class Solution {
         if (SearchCost > k) {
             return 0;
         }
-        if (dp[idx][maxSofar][SearchCost] != -1) {
+        if (dp[idx][maxSofar][SearchCost] != null) {
             return dp[idx][maxSofar][SearchCost];
         }
         int result = 0;
         for (int i = 1; i <= m; i++) {
             if (i > maxSofar) {
-                result = (result + help(n, m, i, k, idx + 1, SearchCost + 1, dp)) % MOD;
+                result = (result + help(n, m, i, k, idx + 1, SearchCost + 1, dp))%MOD ;
             } else {
-                result = (result + help(n, m, maxSofar, k, idx + 1, SearchCost, dp)) % MOD;
+                result = (result + help(n, m, maxSofar, k, idx + 1, SearchCost, dp))%MOD ;
             }
         }
         return dp[idx][maxSofar][SearchCost] = result;
