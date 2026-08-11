21576ed5c66d41169805d252e6d12a4afd966040
HarshitJoshi281 <28harshit98@gmail.com>
2026-08-11 12:42:39 +0530

    Time: 4 ms (9.64%), Space: 50 MB (10.8%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -30,6 +30,9 @@ class Solution {
             curr.next = last;
             last.next = next;
             curr=next;
+            if(curr==null){
+                break;
+            }
         }
 
 
